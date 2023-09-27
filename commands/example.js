7d8b6b148967679b933fe3ea6ab62f952e11478b
Carlo <carloflores953@gmail.com>
2023-09-27 09:37:46 -0400

    Initial commit

---
+++ commands/example.js
@@ -0,0 +1,15 @@
+const logger = require('../logger');
+
+const { SlashCommandBuilder } = require('discord.js');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('example')
+    .setDescription('Executes an example command')
+    .addStringOption(option => 
+      option.setName('prompt')
+      .setDescription('Enter a prompt')
+      .setRequired(true)),
+  async execute(interaction) {
+    await interaction.channel.sendTyping()
+  }
+}
